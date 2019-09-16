d076dc282b4ed29cc02b99919626b7f5d453bd98
Sambhavi Dwivedi <55106165+sdwivedi02@users.noreply.github.com>
2019-09-15 22:36:12 -0400

    ProblemSet0.java
    final commit

---
+++ ProblemSet0.java
@@ -0,0 +1,119 @@
+/**
+ * Problem Set 0.
+ *
+ * It's time to put your skills to the test. This problem set focuses on printing
+ * text to the console, as well as identifying and correcting compilation and
+ * runtime errors in the supplied program code.
+ *
+ * The specifications for each exercise are outlined below. I've written most of
+ * the functionality for you, but unfortunately, my code is riddled with mistakes.
+ * It's your job to fix the errors I've made, so that the code meets the
+ * requirements set forth in this section (as well as the Deliverables section).
+ *
+ * Work through these exercises on your own. Experiment, make mistakes, ask
+ * questions, and fix your mistakes. It's the only way to get good at programming.
+ */
+
+public class ProblemSet0 {
+
+    public static void main(String args[]) {
+
+        /*
+         * Exercise 1.
+         *
+         * Print a message to the console.
+         */
+
+        //System.out.println("Hello, world! Am I a programmer yet?");
+
+         /*
+          * Exercise 2.
+          *
+          * Print APCS in block letters to the console.
+          */
+
+         //System.out.println("   ##    ######  ######## ########");
+         //System.out.println("  #  #   #     # #        #       ");
+         //System.out.println(" #    #  #     # #        #       ");
+         //System.out.println("######## ######  #        ########");
+         //System.out.println("#      # #       #               #");
+         //System.out.println("#      # #       #               #");
+         //System.out.println("#      # #       ######## ########");
+
+         /*
+          * Exercise 3.
+          *
+          * Print a quoted message to the console.
+          */
+
+         //System.out.println("\n\"The answer's quite simple\", the professor said. \"Just think outside the box!\"");
+
+         /*
+          * Exercise 4.
+          *
+          * Print a face to the console.
+          */
+
+         //System.out.println("\n   \\\\\\\\\\");
+         //System.out.println("  +\"\"\"\"\"+");
+        //System.out.println("(|  0 0  |)");
+        //System.out.println(" |   ^   |");
+        //System.out.println(" |  '-'  |");
+        //System.out.println("  +-----+");
+        //
+        // /*
+        //  * Exercise 5.
+        //  *
+        //  * Print a to-do list to the console.
+        //  */
+        //
+        //System.out.println("\nHere's today's to-do list:");
+        //System.out.println("\t -Go food shopping");
+        //System.out.println("\t -Mow the lawn");
+        //
+        // /*
+        //  * Exercise 6.
+        //  *
+        //  * Print a square of text to the console.
+        //  */
+        //
+        //  System.out.println("\nH E L L O\nE\tA\nL\tP\nL\tC\nO A P C S");
+        // /*
+        //  * Exercise 7.
+        //  *
+        //  * Print a countdown to the console.
+        //  */
+        //
+        // System.out.println("\nThree.\tTwo.\tOne.\tGo!\n");        //
+        // /*
+        //  * Exercise 8.
+        //  *
+        //  * Print three diamonds to the console.
+        //  */
+        //
+        //System.out.println("\n  /\\     /\\     /\\");
+        //System.out.println(" /  \\   /  \\   /  \\");
+        //System.out.println("/    \\ /    \\ /    \\");
+        //System.out.println("\\    / \\    / \\    /");
+        //System.out.println(" \\  /   \\  /   \\  /");
+        //System.out.println("  \\/     \\/     \\/");
+        //
+        // /*
+        //  * Exercise 9.
+        //  *
+        //  * Print an encouraging message to the console.
+        //  */
+        //
+        // System.out.println("\nYou're almost done with your first problem set...");
+        //
+        // /*
+        //  * Exercise 10.
+        //  *
+        //  * Print a plus sign to the console.
+        //  */
+        //
+        //System.out.println("\t+---+\n\t|   |");
+        //System.out.println("    +---+   +---+\n    |\t\t|\n    +---+   +---+");
+        //System.out.println("\t|   |\n\t+---+");
+}
+}
